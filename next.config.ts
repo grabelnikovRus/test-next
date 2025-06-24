cb81ea9568dc90403676938e529b6b41e5cb2b23
RG <grabelnikov.rus@mail.ru>
2025-06-24 16:19:40 +0700

    master: fix

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,19 @@ const nextConfig: NextConfig = {
   images: {
     domains: ['placehold.co', "picsum.photos", "dummyimage.com"],
   },
+  async headers() {
+    return [
+      {
+        source: '/(.*)',
+        headers: [
+          {
+            key: 'Content-Security-Policy',
+            value: "upgrade-insecure-requests"
+          }
+        ]
+      }
+    ];
+  }
 };
 
 export default nextConfig;
